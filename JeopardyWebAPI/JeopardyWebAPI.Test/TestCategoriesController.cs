b86eb6a4627ea89de2e2ecadc3d1bcd13f3612b2
hebesister <selina_huixian@hotmail.com>
2020-06-16 15:10:31 -0400

    S5 anderson unit testing (#13)
    * test
    
    * add last successful build
    
    * ignored bin folder
    
    Co-authored-by: Sanderson91 <Sanderson91@users.noreply.github.com>
    Co-authored-by: Selina Xie <selina.xie@canada.ca>

--- JeopardyWebAPI/JeopardyWebAPI.Test/TestCategoriesController.cs
+++ JeopardyWebAPI/JeopardyWebAPI.Test/TestCategoriesController.cs
@@ -1,23 +1,44 @@
 using JeopardyWebAPI.Controllers;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Xunit;
+using Xunit.Sdk;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JeopardyWebAPI.Data.EFCore;
+using Microsoft.AspNetCore.Mvc;
 using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using AutoMapper;
+using JeopardyWebAPI;
 
 namespace JeopardyWebAPI.Test
 {
     [TestClass]
     public class TestCategoriesController
     {
+        CategoriesController _controller;
+        IJeopardyRepository _service;
+        IMapper _mapper;
+
+
+        public TestCategoriesController() {
+
+            _service = new JeopardyRepositoryFake();
+            _controller = new CategoriesController(_service, _mapper);
+
+        }
+
+
         [Fact]
-        [TestMethod]
         public void GetAllCategories_ShouldReturnAllCategories()
         {
-           // var testProducts = GetAll();
-           // var controller = new CategoriesController();
 
-           // var result = controller.Get() as List<CategoriesController>;
-           // Assert.AreEqual(testProducts.Count, result.Count);
+            // Act
+            var okResult = _controller.Get();
+      
+            // Assert
+            Assert.IsNotNull(okResult);
+        
         }
+
+
     }
 }
